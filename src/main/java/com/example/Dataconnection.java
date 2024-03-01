d936765e24e3a0d2ed03f599380e28c739322855
sreeram <ssreeram358@gmail.com>
2024-03-01 12:24:40 +0530

    10

--- src/main/java/com/example/Dataconnection.java
+++ src/main/java/com/example/Dataconnection.java
@@ -1,27 +1,12 @@
 package com.example;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-@WebServlet("/showData")
-public class Dataconnection extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Connection conn = null;
-        Statement stmt = null;
-        ResultSet rs = null;
-
+public class Dataconnection {
+    public static void main(String[] args) {
         try {
             // Load the MySQL driver
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -32,28 +17,31 @@ public class Dataconnection extends HttpServlet {
             String password = "tarka*62M!C8}V]T7U_tRtitpl";
 
             // Connect to the database
-            conn = DriverManager.getConnection(url, username, password);
+            Connection conn = DriverManager.getConnection(url, username, password);
 
             // Create a statement object
-            stmt = conn.createStatement();
+            Statement stmt = conn.createStatement();
 
             // Execute a SELECT query
-            rs = stmt.executeQuery("SELECT sl_no,employee_name FROM tarka");
+            ResultSet rs = stmt.executeQuery("SELECT sl_no,employee_name FROM tarka");
 
-            // Store the results in the request object
-            request.setAttribute("results", rs);
+            // Process the result set
+            while (rs.next()) {
+                int sl_no = rs.getInt("sl_no");
+                String employee_name = rs.getString("employee_name");
+                // int age = rs.getInt("age");
 
-            // Forward the request to a JSP page
-            RequestDispatcher rd = request.getRequestDispatcher("/showData.jsp");
-            rd.forward(request, response);
+                // Print the retrieved data
+                System.out.println("sl_no: " + sl_no + ", employee_name: " + employee_name );
+            }
+
+            // Close the resources
+            rs.close();
+            stmt.close();
+            conn.close();
 
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            // Close the resources
-            try { rs.close(); } catch (Exception e) {}
-            try { stmt.close(); } catch (Exception e) {}
-            try { conn.close(); } catch (Exception e) {}
         }
     }
 }
