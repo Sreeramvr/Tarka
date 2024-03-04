789589453268d03e6e0e6971a4423298feea3977
sreeram <ssreeram358@gmail.com>
2024-03-04 10:27:37 +0530

    11

---
+++ src/main/java/com/example/DisplayDataServlet.java
@@ -0,0 +1,73 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/display-data")
+public class DisplayDataServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set the response MIME type to HTML
+        response.setContentType("text/html;charset=UTF-8");
+
+        // Get a PrintWriter for writing the response
+        PrintWriter out = response.getWriter();
+
+        // Write the response message, in an HTML page
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html><head>");
+            out.println("<title>Display Data</title></head>");
+            out.println("<body>");
+
+            // Load the MySQL JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Connect to the database
+            Connection conn = DriverManager.getConnection("jdbc:mysql://13.232.155.0/vyapak", "tarka", "tarka*62M!C8}V]T7U_tRtitpl");
+
+            // Create a Statement object
+            Statement stmt = conn.createStatement();
+
+            // Execute a SQL query to retrieve data from the "employees" table
+            ResultSet rs = stmt.executeQuery("SELECT sl_no,employee_name FROM tarka");
+
+            // Display the data in an HTML table
+            out.println("<table border='1'>");
+            out.println("<tr><th>SLNO</th><th>EMPLOYEENAME</th></tr>");
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+//                String position = rs.getString("position");
+//                double salary = rs.getDouble("salary");
+                out.println("<tr><td>" + id + "</td><td>" + name + "</td></tr>");
+            }
+            out.println("</table>");
+
+            // Close the ResultSet, Statement, and Connection objects
+            rs.close();
+            stmt.close();
+            conn.close();
+
+            out.println("</body></html>");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            out.println("<p>Error: " + e.getMessage() + "</p>");
+        }
+
+    }
+
+}
